206ce9e81bb261e57736c5780f842113e536edde
root <root@demo-projec-1-acs-bootstrap-hrclm>
2024-11-01 09:49:43 +0000

    Initial commit

---
+++ src/main/java/com/example/DemoProjec1BffApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjec1BffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjec1BffApplication.class, args);
+	}
+
+}
